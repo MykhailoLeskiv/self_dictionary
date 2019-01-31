306441c7cd6add88e5bdc5bc02e881630d10ed20
mykhailoleskiv <daniskort@ukr.net>
2019-01-31 12:01:27 +0200

    small fixes

--- app/models.py
+++ app/models.py
@@ -12,8 +12,8 @@ def load_user(id):
 class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(128), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(128), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128))
 
     def set_password(self, password):
@@ -29,9 +29,9 @@ class User(db.Model, UserMixin):
 class Dictionary(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    native_lang = db.Column(db.String(32), index=True)
-    foreign_lang = db.Column(db.String(32), index=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    native_lang = db.Column(db.String(32), index=True, nullable=False)
+    foreign_lang = db.Column(db.String(32), index=True, nullable=False)
 
     def __repr__(self):
         return '<{} - {} dictionary>'.format(self.native_lang, self.foreign_lang)
@@ -40,9 +40,9 @@ class Dictionary(db.Model):
 class Chapter(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    dictionary = db.Column(db.Integer,  db.ForeignKey('dictionary.id'))
+    dictionary = db.Column(db.Integer,  db.ForeignKey('dictionary.id'), nullable=False)
     created_datetime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    chapter_name = db.Column(db.String(32), index=True, unique=True)
+    chapter_name = db.Column(db.String(32), index=True, unique=True, nullable=False)
 
     def __repr__(self):
         return '<Chapter name is {}>'.format(self.chapter_name)
@@ -51,9 +51,9 @@ class Chapter(db.Model):
 class Word(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    chapter = db.Column(db.Integer,  db.ForeignKey('chapter.id'))
-    word = db.Column(db.String(64))
-    translation = db.Column(db.String(64))
+    chapter = db.Column(db.Integer,  db.ForeignKey('chapter.id'), nullable=False)
+    word = db.Column(db.String(64), nullable=False)
+    translation = db.Column(db.String(64), nullable=False)
     created_datetime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __rerp__(self):
